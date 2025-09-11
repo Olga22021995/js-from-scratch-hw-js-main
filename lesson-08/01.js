88cb5440f7d129d0191641aad2070cb7f505eec0
Olga22021995 <nedvetskaya.1995@gmail.com>
2025-09-11 15:05:58 +0300

    commit

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,37 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+
+function capitalizeWords() {
+    // Если строка пустая, возвращаем пустую строку
+    if (str.length === 0) return '';
+    
+    let result = '';
+    let capitalizeNext = true;
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+        
+        // Если нужно capitalize следующий символ и это буква
+        if (capitalizeNext && currentChar !== ' ') {
+            result += currentChar.toUpperCase();
+            capitalizeNext = false;
+        } else {
+            result += currentChar;
+        }
+        
+        // Если текущий символ - пробел, следующий символ нужно capitalize
+        if (currentChar === ' ') {
+            capitalizeNext = true;
+        }
+    }
+    
+    return result;
+}
+
+// Тестирование функции
+console.log(capitalizeWords("hello world from javascript")); // "Hello World From Javascript"
+console.log(capitalizeWords("  multiple   spaces  here  ")); // "  Multiple   Spaces  Here  "
+console.log(capitalizeWords("")); // ""
+console.log(capitalizeWords("single")); // "Single"
+console.log(capitalizeWords("a b c d")); // "A B C D"
